4336c88bad540d773fb24874cb5fd9ead6500022
Gustavo <versace123mx@gmail.com>
2024-10-03 23:32:12 -0600

    agregando un context para el inicio de session

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -6,27 +6,30 @@ import Register from '../components/user/Register'
 import PrivateLayout from '../components/layout/private/PrivateLayout'
 import Feed from '../components/publication/Feed'
 import Error404 from '../components/layout/public/Error404'
+import { AuthProvider } from '../context/AuthProvider'
 
 const Routing = () => {
   return (
     <BrowserRouter>
+      <AuthProvider>
         <Routes>
-            <Route path='/' element={<PublicLayout />} >{/*Carga la barra de header en todo momento ya que este es el contenedor principal*/}
-                <Route index element={<Login />} />
-                <Route path='login' element={<Login />} />
-                <Route path='registro' element={<Register />} />
-                <Route path='*' element={<Error404 />} />
-            </Route>
-
-            {/**Aqui van las rutas privadas */}
-            <Route path='/social' element={<PrivateLayout />}> 
-              <Route index element={<Feed />} /> {/*Este componente carga al entrar a /social */}
-              <Route path='feet' element={<Feed />} />{/*Este componente carga al entrar a /social/feet */}
-              <Route path='*' element={<Error404 />} />
-            </Route>
-
+          <Route path='/' element={<PublicLayout />} >{/*Carga la barra de header en todo momento ya que este es el contenedor principal*/}
+            <Route index element={<Login />} />
+            <Route path='login' element={<Login />} />
+            <Route path='registro' element={<Register />} />
             <Route path='*' element={<Error404 />} />
+          </Route>
+
+          {/**Aqui van las rutas privadas */}
+          <Route path='/social' element={<PrivateLayout />}>
+            <Route index element={<Feed />} /> {/*Este componente carga al entrar a /social */}
+            <Route path='feet' element={<Feed />} />{/*Este componente carga al entrar a /social/feet */}
+            <Route path='*' element={<Error404 />} />
+          </Route>
+
+          <Route path='*' element={<Error404 />} />
         </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
